640c628643858cadaaefe69770202d92b6c411cf
qinhui <932433511@qq.com>
2020-06-30 15:25:34 +0800

    feat: 修改 mapTree 方法

--- src/util/mapTree.ts
+++ src/util/mapTree.ts
@@ -6,20 +6,31 @@ function mapTree(
   treeData: any,
   mapCallback: any,
   parentDepth: number = -1,
-  serialIndex: number = 0
+  serialIndex: number = 0,
+  parentTopicIndex: number = -1
 ) {
   const { children, ...rest } = treeData
-
+  let topicIndex: number = parentTopicIndex
+  if (parentDepth < 0) {
+    topicIndex = -1
+  } else if (parentDepth === 0) {
+    topicIndex = serialIndex
+  } else {
+    topicIndex = parentTopicIndex
+  }
   const target: any = {
     depth: parentDepth + 1, // 深度
     serialIndex, // 同级的 index
+    topicIndex,
     ...mapCallback(rest) // map 的回调
   }
 
   if (children && Array.isArray(children) && children.length > 0) {
     target.children = []
     children.forEach((item, index: number) => {
-      target.children.push(mapTree(item, mapCallback, parentDepth + 1, index))
+      target.children.push(
+        mapTree(item, mapCallback, parentDepth + 1, index, topicIndex)
+      )
     })
   }
   return target
