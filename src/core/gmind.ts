19bbc674490fb7f74ea183b7f3a1d360d3fa3a8e
qinhui <932433511@qq.com>
2020-07-05 21:41:38 +0800

    feat: modify class Gmind

--- src/core/gmind.ts
+++ src/core/gmind.ts
@@ -3,9 +3,79 @@ import G6 from '@antv/g6'
 import { GraphOptions } from '@antv/g6/lib/interface/graph'
 
 export default class Gmind extends G6.TreeGraph {
-  // eslint-disable-next-line no-useless-constructor
   constructor(cfg: GraphOptions) {
-    super(cfg)
+    super({
+      modes: {
+        default: [
+          {
+            type: 'drag-canvas',
+            enableOptimize: true
+          },
+          'zoom-canvas',
+          'click-select',
+          {
+            type: 'brush-select',
+            trigger: 'shift',
+            includeEdges: false,
+            // @ts-ignore
+            brushStyle: {
+              stroke: '#18a0fb',
+              fill: '#18a0fb',
+              fillOpacity: 0.2,
+              lineWidth: 0.3
+            }
+          },
+          // 展开收起
+          {
+            type: 'collapse-expand',
+            trigger: 'click',
+            onChange: (item, collapsed) => {
+              // @ts-ignore
+              const data = item.get('model').data
+              data.collapsed = collapsed
+              return true
+            }
+          }
+        ]
+      },
+      defaultNode: {
+        type: 'base-node',
+        size: [200, 100]
+      },
+      defaultEdge: {
+        type: 'curveline',
+        style: {
+          stroke: '#A3B1BF'
+        }
+      },
+      layout: {
+        type: 'mindmap',
+        direction: 'LR',
+        getHeight: function getHeight(d: { lines: any[] }) {
+          if (d.lines.length > 0) {
+            return 40 + (d.lines.length - 1) * 16
+          }
+          return 40
+        },
+        // 指定节点宽度
+        getWidth: function getWidth() {
+          return 50
+        },
+        // 指定节点之间的垂直间距
+        getVGap: function getVGap() {
+          return 5
+        },
+        // 指定节点之间的水平间距
+        getHGap: function getHGap(d: { depth: any }) {
+          const { depth } = d
+          if (depth === 0) {
+            return 140
+          }
+          return 100
+        }
+      },
+      ...cfg
+    })
   }
 
   getData() {
--- src/index.tsx
+++ src/index.tsx
@@ -38,79 +38,7 @@ const Viewer: FC<IViewerProps> = (props) => {
       // @ts-ignore
       width: designerRef.current.offsetWidth,
       // @ts-ignore
-      height: designerRef.current.offsetHeight,
-      modes: {
-        default: [
-          {
-            type: 'drag-canvas',
-            enableOptimize: true
-          },
-          'zoom-canvas',
-          'click-select',
-          // 框选
-          {
-            type: 'brush-select',
-            trigger: 'shift',
-            includeEdges: false,
-            // @ts-ignore
-            brushStyle: {
-              stroke: '#18a0fb',
-              fill: '#18a0fb',
-              fillOpacity: 0.2,
-              lineWidth: 0.3
-            }
-          },
-          // 展开收起
-          {
-            type: 'collapse-expand',
-            trigger: 'click',
-            onChange: (item, collapsed) => {
-              // @ts-ignore
-              const data = item.get('model').data
-              data.collapsed = collapsed
-              return true
-            }
-          }
-        ]
-      },
-      defaultNode: {
-        type: 'base-node',
-        size: [200, 100]
-      },
-      defaultEdge: {
-        type: 'curveline',
-        style: {
-          stroke: '#A3B1BF'
-        }
-      },
-      layout: {
-        type: 'mindmap',
-        direction: 'LR',
-        getHeight: function getHeight(d: { lines: any[] }) {
-          if (d.lines.length > 0) {
-            return 40 + (d.lines.length - 1) * 16
-          }
-          return 40
-        },
-        // 指定节点宽度
-        getWidth: function getWidth() {
-          // debugger
-          return 50
-        },
-        // 指定节点之间的垂直间距
-        getVGap: function getVGap() {
-          // debugger
-          return 5
-        },
-        // 指定节点之间的水平间距
-        getHGap: function getHGap(d: { depth: any }) {
-          const { depth } = d
-          if (depth === 0) {
-            return 140
-          }
-          return 100
-        }
-      }
+      height: designerRef.current.offsetHeight
     })
 
     const targetData = mapTree(sourceData, function (item: any) {
@@ -144,4 +72,4 @@ const Viewer: FC<IViewerProps> = (props) => {
   )
 }
 
-export { Viewer }
+export { Viewer, Gmind }
